393ac345c79ed6a719b218502118652bab4b5fd2
Administrator <admin@example.com>
2017-11-13 15:20:54 +0530

    Created test app

---
+++ src/app/manage-book/update-book.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import 'rxjs/add/operator/switchMap';
+
+import { Book } from '../services/book';
+import { BookService } from '../services/book.service';
+
+@Component({
+    selector: 'app-update-book-app',
+    templateUrl: './update-book.component.html',
+    styleUrls: ['./update-book.component.css']
+})
+export class UpdateBookComponent implements OnInit {
+    book: Book = new Book();
+    constructor(private route: ActivatedRoute,
+        private bookService: BookService,
+        private location: Location) { }
+    ngOnInit(): void {
+        this.route.params
+            .switchMap((params: Params) => this.bookService.getBook(+params['id']))
+            .subscribe(book => this.book = book);
+    }
+    goBack(): void {
+        this.location.back();
+    }
+}
